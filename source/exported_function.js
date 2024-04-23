b733e6de38b948ff43d6acb5899b1ad726d28cc5
theo-baccam <theo.baccam@laplateforme.io>
2024-04-23 15:58:28 +0200

    test: simple exported function for displaying image

---
+++ source/exported_function.js
@@ -0,0 +1,8 @@
+function exported_function() {
+    /* you need to specify image path relative to root
+    folder if index in root
+    */
+    $("body").append("<img src='assets/00.png'></img>")
+}
+
+export {exported_function}
