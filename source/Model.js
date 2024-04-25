bf74c5f7cc4b4388f2522085935fcad413ac6994
theo-baccam <theo.baccam@laplateforme.io>
2024-04-25 14:51:06 +0200

    feat(Model): new game methods

--- source/Model.js
+++ source/Model.js
@@ -1,23 +1,58 @@
+import {Element} from "./Element.js"
+
+
 class Model {
-    constructor(
-        points,
-        DEFAULT_CLICK_VALUE,
-        clickValueUpgrades,
-        DEFAULT_BOOST_CHANCE,
-        boostChanceUpgrades,
-    ) {
-        this.points = points
+    constructor() {
+        console.log("Initializing Model")
 
-        this.DEFAULT_CLICK_VALUE = DEFAULT_CLICK_VALUE;
-        this.clickValueUpgrades = clickValueUpgrades;
+        this.points;
 
-        this.DEFAULT_BOOST_CHANCE = DEFAULT_BOOST_CHANCE;
-        this.boostChanceUpgrades = boostChanceUpgrades;
+        this.DEFAULT_CLICK_VALUE;
+        this.clickValueUpgrades;
 
-        this.elements = [];
+        this.DEFAULT_BOOST_CHANCE;
+        this.boostChanceUpgrades;
+
+        this.elements;
+    }
+
+    isNewGame() {
+        console.log("Checking if new game")
+        let modelVariables = [
+            this.points,
+            this.DEFAULT_CLICK_VALUE,
+            this.clickValueUpgrades,
+            this.DEFAULT_BOOST_CHANCE,
+            this.boostChanceUpgrades,
+            this.elements,
+        ];
+
+        for (let i = 0; i < modelVariables; i++) {
+            if (localStorage.getItem(modelVariables[i]) === null) {
+                console.log(`${modelVariables[i]} is null`)
+                this.newGameState();
+                return;
+            };
+        };
+    }
+
+    newGameState() {
+        console.log("Creating new game save")
+        this.points = 0;
+
+        this.DEFAULT_CLICK_VALUE = 1;
+        this.clickValueUpgrades = 0;
+
+        this.DEFAULT_BOOST_CHANCE = 1;
+        this.boostChanceUpgrades = 0;
+
+        this.elements = []
+
+        this.saveGameState();
     }
 
     saveGameState() {
+        console.log("Saving game state")
         localStorage.setItem("points", this.points);
 
         localStorage.setItem("DEFAULT_CLICK_VALUE", this.DEFAULT_CLICK_VALUE);
@@ -29,56 +64,3 @@ class Model {
         localStorage.setItem("elements", this.elements)
     }
 }
-
-
-// local_storage.js
-
-// Define a key for local storage to store the game state
-// const GAME_STATE_KEY = 'gameState';
-
-// Function to save the game state to local storage
-// function saveGameState(gameState) {
-//     try {
-//         // Convert the game state object to a JSON string
-//         const gameStateJson = JSON.stringify(gameState);
-//         // Save the JSON string to local storage
-//         localStorage.setItem(GAME_STATE_KEY, gameStateJson);
-//         console.log('Game state saved to local storage.');
-//     } catch (error) {
-//         console.error('Failed to save game state:', error);
-//     }
-// }
-
-// Function to load the game state from local storage
-// function loadGameState() {
-//     try {
-//         // Retrieve the JSON string from local storage
-//         const gameStateJson = localStorage.getItem(GAME_STATE_KEY);
-//         if (gameStateJson) {
-//             // Parse the JSON string to a game state object
-//             const gameState = JSON.parse(gameStateJson);
-//             console.log('Game state loaded from local storage.');
-//             return gameState;
-//         } else {
-//             console.log('No game state found in local storage.');
-//             return null;
-//         }
-//     } catch (error) {
-//         console.error('Failed to load game state:', error);
-//         return null;
-//     }
-// }
-// 
-// // Function to clear the game state from local storage
-// function clearGameState() {
-//     try {
-//         // Remove the game state from local storage
-//         localStorage.removeItem(GAME_STATE_KEY);
-//         console.log('Game state cleared from local storage.');
-//     } catch (error) {
-//         console.error('Failed to clear game state:', error);
-//     }
-// }
-// 
-// // Export the functions to make them available for import in other files
-// export { saveGameState, loadGameState, clearGameState };
