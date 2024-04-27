df095d96699711c0801f32fc74ef237222a350a3
theo-baccam <theo.baccam@laplateforme.io>
2024-04-27 17:15:48 +0200

    feat: element buying

--- source/Controller.js
+++ source/Controller.js
@@ -16,6 +16,7 @@ class Controller {
         this.bindClickerClick();
         this.pointsPerSecondsInterval();
         this.displayElements();
+        this.bindBuyButtons();
     }
 
     bindClickerClick() {
@@ -40,9 +41,29 @@ class Controller {
 
     displayElements() {
         for (let i = 0; i < this.model.elements.length; i++) {
-            let element = this.model.elements[i]
-            this.view.displayElement(element)
+            let element = this.model.elements[i];
+            this.view.displayElement(element);
         }
+    }
+
+    bindBuyButtons() {
+        for (let i = 0; i < this.model.elements.length; i++) {
+            let element = this.model.elements[i];
+            let buyButton = document.getElementById(`${element.name}BuyButton`);
+            buyButton.addEventListener("click", async () => {
+                let elementPrice = (
+                    element.DEFAULT_PRICE
+                    * Math.pow(1.6, element.amount)
+                );
+                if (elementPrice <= this.model.points) {
+                    this.model.points -= elementPrice;
+                    element.amount++;
+                    await this.model.saveGameState();
+                    this.view.displayTotalPoints(this.model.points);
+                    this.view.displayElementAmount(element);
+                };
+            });
+        };
     }
 }
 
--- source/View.js
+++ source/View.js
@@ -26,7 +26,7 @@ class View {
         elementDiv.appendChild(elementDisplayDiv);
 
         let elementButtonsDiv = document.createElement("div");
-        elementButtonsDiv.className = "elementButtonDiv";
+        elementButtonsDiv.className = "elementButtonsDiv";
         elementButtonsDiv.id = `${element.name}ButtonsDiv`;
         const width = elementDisplayDiv.offsetHeight / 3;
         elementButtonsDiv.style.width = `${width}px`;
@@ -34,15 +34,25 @@ class View {
 
         let elementBuyButton = document.createElement("button");
         elementBuyButton.className = "elementButton"
+        elementBuyButton.id = `${element.name}BuyButton`
         elementButtonsDiv.appendChild(elementBuyButton);
 
         let elementSellButton = document.createElement("button");
         elementSellButton.className = "elementButton"
+        elementSellButton.id = `${element.name}SellButton`
         elementButtonsDiv.appendChild(elementSellButton);
 
         let elementUpgradeButton = document.createElement("button");
         elementUpgradeButton.className = "elementButton"
+        elementUpgradeButton.id = `${element.name}UpgradeButton`
         elementButtonsDiv.appendChild(elementUpgradeButton);
+    }
+
+    displayElementAmount(element) {
+        let elementDisplayDiv = (
+            document.getElementById(`${element.name}DisplayDiv`)
+        );
+        elementDisplayDiv.innerHTML = `${element.name} ${element.amount}`;
     }
 }
 
